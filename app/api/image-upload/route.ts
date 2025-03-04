bfd53d183fb4c50e92bd99a15f6a3fe22f7496ce
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-03-04 00:04:26 -0700

    cloudinary connected and video upload logic done

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -19,9 +19,45 @@ interface CloudinaryUploadResult {
 
 
 export async function POST(request: NextRequest) {
-    const [userId] = auth()
+    const [userId] = auth();
 
     if (!userId) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+    }
+
+    try {
+        const formData = await request.formData();
+        console.log(formData)
+        const file = formData.get("file") as File | null;
+
+        if (!file) {
+            return NextResponse.json({ error: "File not found" }, { status: 400 });
+        }
+
+        const bytes = await file.arrayBuffer()
+        const buffer = Buffer.from(bytes)
+
+        const result = await new Promise<CloudinaryUploadResult>(
+            (resolve, reject) => {
+                const uploadStream = cloudinary.uploader.upload_stream(
+                    { folder: "next-cloudinary-uploads" },
+                    (error, result) => {
+                        if (error) reject(error);
+                        else resolve(result as CloudinaryUploadResult);
+                    }
+                )
+                uploadStream.end(buffer)
+            }
+        )
+        return NextResponse.json(
+            {
+                publicId: result.public_id
+            },
+            {
+                status: 200
+            })
+    } catch (error) {
+        console.log("Upload image failed", error)
+        return NextResponse.json({ error: "Upload image failed" }, { status: 500 })
     }
 }
